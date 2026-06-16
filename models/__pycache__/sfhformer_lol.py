b87094ab8327e3d45e2a4fe619a05f7d970c0ef4
zhushuo <zhushuo@stu.hist.edu.cn>
2026-06-16 23:28:04 +0800

    2

---
+++ models/__pycache__/sfhformer_lol.py
@@ -0,0 +1,9 @@
+# decompyle3 version 3.9.3
+# Python bytecode version base 3.10 (3439)
+# Decompiled from: Python 3.10.14 (main, May  6 2024, 19:42:50) [GCC 11.2.0]
+# Embedded file name: /root/lanyun-tmp/lowlight_baseline+Rstb-Copy2/models/sfhformer_lol.py
+# Compiled at: 2025-11-27 20:31:35
+# Size of source mod 2**32: 31903 bytes
+
+Unsupported Python version, 3.10, for decompilation
+
